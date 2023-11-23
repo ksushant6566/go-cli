af51e08c04298cd62178bbd2b8d4c8bcc1f5d96b
SUSHANT KUMAR <ksushant6566@gmail.com>
2023-11-24 00:19:25 +0530

    replaced string sclice with UserData Struct

--- main.go
+++ main.go
@@ -5,7 +5,13 @@ import (
 	"go-cli/helper"
 )
 
-var bookings = []string{}
+type UserData struct {
+	userName string
+	email string
+	numberOfTickets uint
+}
+
+var bookings = make([]UserData, 0)
 const conferenceName = "Golang Conference"
 const conferenceTickets uint = 50
 var remainingTickets = conferenceTickets
@@ -32,8 +38,14 @@ func main () {
 }
 
 func bookTickets(username, email string, numberOfTickets uint) uint {
+
+	userData := new(UserData)
+
+	userData.userName = username
+	userData.email = email
+	userData.numberOfTickets = numberOfTickets
 	
-	bookings = append(bookings, username)
+	bookings = append(bookings, *userData)
 	remainingTickets -= numberOfTickets
 
 	fmt.Printf("\nThankyou %v, you have booked %v tickets!\nYou will recieve an email confirmation at %v\n", username, numberOfTickets, email)
@@ -41,8 +53,17 @@ func bookTickets(username, email string, numberOfTickets uint) uint {
 	if remainingTickets == 0 {
 		fmt.Println("All tickets for this conference are sold out!")
 	} else {
-		fmt.Printf("%v tickets remaining\n", remainingTickets)
+		printBookingSummary()
 	}
 
 	return remainingTickets
+}
+
+func printBookingSummary() {
+	fmt.Println("Booking summary")
+	for _, booking := range bookings {
+		fmt.Printf("\nusername: %v, email: %v, tickets: %v.", booking.userName, booking.email, booking.numberOfTickets)
+	}
+	fmt.Println("")
+	fmt.Println("Booking summary end")
 }
